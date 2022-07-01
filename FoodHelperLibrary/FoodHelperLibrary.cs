c8213808053902b6651db13a83a55fe289823b62
Сергей <s3rg0sh4@gmail.com>
2022-07-01 23:08:04 +0300

    Add create and adduser queries

--- FoodHelperLibrary/FoodHelperLibrary.cs
+++ FoodHelperLibrary/FoodHelperLibrary.cs
@@ -12,24 +12,83 @@ namespace FoodHelperLibrary
 {
     public static class FoodHelperDB
     {
+        readonly static string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "FoodHelper.db");
         public async static void InitializeDatabase()
         {
             await ApplicationData.Current.LocalFolder.CreateFileAsync("FoodHelper.db", CreationCollisionOption.OpenIfExists);
-            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "FoodHelper.db");
-            using (SqliteConnection db =
-               new SqliteConnection($"Filename={dbpath}"))
+            //string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "FoodHelper.db");
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
             {
                 db.Open();
 
-                String tableCommand = "CREATE TABLE IF NOT " +
-                    "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
-                    "Text_Entry NVARCHAR(2048) NULL)";
+                String tableCommand =
+                    "CREATE TABLE IF NOT EXISTS " +
+                    "Ingredients (" +
+                    "ingredientID INTEGER PRIMARY KEY NOT NULL, " +
+                    "ingredientName CHARACTER(30) NOT NULL, " +
+                    "calories DOUBLE NOT NULL, " +
+                    "proteins DOUBLE NOT NULL, " +
+                    "fats DOUBLE NOT NULL, " +
+                    "carbs DOUBLE NOT NULL" +
+                    ");" +
+                    "CREATE TABLE IF NOT EXISTS " +
+                    "Recipies (" +
+                    "recipeID INTEGER PRIMARY KEY NOT NULL, " +
+                    "recipeName CHARACTER(50) NOT NULL, " +
+                    "weight DOUBLE NOT NULL" +
+                    ");" +
+                    "CREATE TABLE IF NOT EXISTS " +
+                    "Ingredients_Recipes (" +
+                    "ingredientID INTEGER NOT NULL, " +
+                    "recipeID INTEGER NOT NULL, " +
+                    "weight DOUBLE NOT NULL, " +
+                    "FOREIGN KEY (ingredientID) REFERENCES Ingredients(ingredientID), " +
+                    "FOREIGN KEY (recipeID) REFERENCES Recipies(recipeID), " +
+                    "PRIMARY KEY (ingredientID, recipeID)" +
+                    ");" +
+                    "CREATE TABLE IF NOT EXISTS " +
+                    "Users (" +
+                    "userID INTEGER PRIMARY KEY NOT NULL, " +
+                    "login CHARACTER(20) NOT NULL, " +
+                    "password CHARACTER(20) NOT NULL" +
+                    ");" +
+                    "CREATE TABLE IF NOT EXISTS " +
+                    "Users_Recepies (" +
+                    "userID INTEGER NOT NULL, " +
+                    "recipeID INTEGER NOT NULL, " +
+                    "date DATE NOT NULL, " +
+                    "count INTEGER, " +
+                    "FOREIGN KEY (userID) REFERENCES Users(userID), " +
+                    "FOREIGN KEY (recipeID) REFERENCES Recipies(recipeID), " +
+                    "PRIMARY KEY (userID, recipeID, date)" +
+                    ");" +
+                    "CREATE TABLE IF NOT EXISTS " +
+                    "Burned (" +
+                    "burnedID INTEGER PRIMARY KEY NOT NULL, " +
+                    "calories DOUBLE, " +
+                    "date DATE, " +
+                    "userID INTEGER NOT NULL, " +
+                    "FOREIGN KEY (userID) REFERENCES Users(userID)" +
+                    ");";
 
                 SqliteCommand createTable = new SqliteCommand(tableCommand, db);
 
                 createTable.ExecuteReader();
             }
         }
+
+        public static void AddUser(string login, string password)
+		{
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+			{
+                db.Open();
+                SqliteCommand addUser = new SqliteCommand();
+                addUser.Connection = db;
+                addUser.CommandText = "INSERT INTO Users VALUES(@login,@password)";
+                addUser.Parameters.AddWithValue("@login", login);
+                addUser.Parameters.AddWithValue("@password", password);
+			}
+		}
     }
 
 }
