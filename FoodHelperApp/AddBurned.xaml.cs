bca3b8f4c51adcea03ce95fd9c123a1c34df5a37
Сергей <s3rg0sh4@gmail.com>
2022-07-04 23:06:46 +0300

    add functionality to addburned

--- FoodHelperApp/AddBurned.xaml.cs
+++ FoodHelperApp/AddBurned.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using FoodHelperLibrary;
+using System.Drawing;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -29,7 +31,20 @@ namespace FoodHelperApp
 
 		private void AddBurned_Click(object sender, RoutedEventArgs e)
 		{
-
+            try
+            {
+                FoodHelperDB.AddUserStatsBurned(int.Parse(Cals.Text), 1);
+                Cals.PlaceholderForeground = new SolidColorBrush(Windows.UI.Color.FromArgb(255,50,50,50));
+                Frame.GoBack();
+            } catch (FormatException)
+			{
+                Cals.Text = "";
+                Cals.PlaceholderText = "Введите число";
+                Cals.PlaceholderForeground = new SolidColorBrush(Windows.UI.Colors.Red);
+            }
 		}
+
+		private void Close_Click(object sender, RoutedEventArgs e) => Frame.GoBack();
+
 	}
 }
